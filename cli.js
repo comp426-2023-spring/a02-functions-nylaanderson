3b6b28424969646711b73f0e322a576f0e0306ba
nylaanderson <69765415+nylaanderson@users.noreply.github.com>
2023-02-09 16:58:10 -0500

    Completed the help text section

---
+++ cli.js
@@ -0,0 +1,37 @@
+#!/user/bin/env node
+
+import minimist from 'minimist';
+
+cont args = minimist(process.argv.slice(2));
+
+if (args.h) {
+	console.log("Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE");
+	console.log("-h            Show this help message and exit.");
+	console.log("-n, -s        Latitude: N positive; S negative.");
+	console.log("-e, -w        Longitude: E positive; W negative.");
+	console.log("-z            Time zone: uses tz.guess() from moment-timezone by default.");
+	console.log("-d 0-6        Day to retrieve weather: 0 is today; defaults to 1.");
+	console.log(" -j            Echo pretty JSON from open-meteo API and exit.");
+	process.exit(0);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
