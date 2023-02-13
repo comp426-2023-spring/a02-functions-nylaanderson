0d22ef31a79fa7b39d056605abd314c47f0fe134
nylaanderson <69765415+nylaanderson@users.noreply.github.com>
2023-02-13 11:53:18 -0500

    Tried reformatting cli.js and tested for incorrect values

--- cli.js
+++ cli.js
@@ -4,7 +4,7 @@ import minimist from 'minimist';
 
 import moment from 'moment-timezone';
 
-cont args = minimist(process.argv.slice(2));
+var args = minimist(process.argv.slice(2));
 
 if (args.h) {
 	console.log("Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE");
@@ -17,13 +17,15 @@ if (args.h) {
 	process.exit(0);
 }
 
-const timezone = moment.tz.guess();
+var timezone = moment.tz.guess();
 let latitude;
 let longitude;
-const days = args.d
 
 if (args.z) {
 	timezone = args.z
+}
+else{
+	timezone = tz;
 }
 
 if (args.n) {
@@ -31,7 +33,7 @@ if (args.n) {
 }
 
 if (args.s) {
-	latitude = -args.s
+	latitude = -1 * args.s
 }
 
 if (args.e) {
@@ -39,12 +41,17 @@ if (args.e) {
 }
 
 if (args.w) {
-	longitude = -args.w
+	longitude = -1 * args.w
+}
+
+if (!latitude && !longitude){
+	console.log("Values must be in range");
+	process.exit(0);
 }
 
 //Make a request
 
-const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude='+ latitude + '&longitude' + longitude + '&daily=precipitation_hours&current_weather=true&temperature_unit=fahrenheit&timezone=' + timezone)
+var response = await fetch('https://api.open-meteo.com/v1/forecast?latitude='+ latitude + '&longitude' + longitude + '&daily=precipitation_hours&current_weather=true&timezone=' + timezone);
 
 const data = await response.json();
 
@@ -54,27 +61,36 @@ if (args.j) {
         process.exit(0);
 }
 
+let days; 
+
+if (args.d == null) {
+	days = 1;
+} else {
+	days = args.d;
+}
+
 if (days == 0) {
-	(console.log("today.")
-	if(data.precipitation_hours[0] > 0) {
-		console.log("You might need your galoshes")
+	console.log("today.");
+	if(data.daily.precipitation_hours[0] > 0) {
+	 	console.log("You might need your galoshes");	
+	}  else{
+	console.log("You will not need your galoshes");
 	}
-	else{
-		console.log("You will not need your galoshes")
-	}
-} else if (days > 1) {
-	console.log("in " + days + " days.")
-	if(data.precipitation_hours[days] > 0) {
-		console.log("You might need your galoshes")
+}
+else if (days > 1) {
+	console.log("in " + days + " days.");
+	if( data.daily.precipitation_hours[days] > 0) {
+		console.log("You might need your galoshes");
 	} else{
-		console.log("You will not need your galoshes"
+	console.log("You will not need your galoshes")
 	} 	
 } else { 
-	console.log("tomorrow.")
-	if(data.precipitation_hours[1] > 0) {
-		console.log("You might need your galoshes")
+	console.log("tomorrow.");
+	console.log(data);
+	if(data.daily.precipitation_hours[1] > 0) {
+		console.log("You might need your galoshes");
 	} else{
-		console.log("You will not need your galoshes")
+		console.log("You will not need your galoshes");
 	}
 }
 
